d11433ccf2e57e66a8f8b272e111d007dfba6cb7
fakhir96 <fakhir9101@gmail.com>
2025-07-23 11:51:09 +0500

    Finaized Filters in To Do List

--- Projects/To Do List/app.js
+++ Projects/To Do List/app.js
@@ -4,13 +4,6 @@ const ul = document.querySelector(".task-list")
 const filters = document.querySelectorAll(".trans")
 const clrBtn = document.querySelector("#clear-btn")
 
-filters.forEach(el => {
-    el.addEventListener("click", ()=>{
-        
-        filters.forEach(btn => btn.classList.remove("act"));
-        el.classList.toggle("act");
-    })
-})
 
 // Save data to localStorage
 const saveData = () => {
@@ -104,10 +97,55 @@ const addTask = ()=>{
     
 }
 
+filters.forEach(el => {
+    el.addEventListener("click", ()=>{
+        
+        filters.forEach(btn => btn.classList.remove("act"));
+        el.classList.toggle("act");
+        console.log(el.innerText)
+
+        const allTasks = ul.querySelectorAll("li");
+
+        if(el.innerText === "Completed"){
+           allTasks.forEach(li => {
+            const checkIcon = li.querySelector(".check-icon");
+            if (checkIcon.classList.contains("checked")) {
+                li.style.display = "flex";  // show completed
+            } 
+            else {
+                li.style.display = "none";  // hide incomplete
+            }
+            });
+        }
+        else if (el.innerText === "Active") {
+            allTasks.forEach(li => {
+                const checkIcon = li.querySelector(".check-icon");
+                if (checkIcon.classList.contains("checked")) {
+                    li.style.display = "none";  // hide completed
+                } 
+                else {
+                    li.style.display = "flex";  // show incomplete
+                }
+            });
+        }
+        else { // All
+            allTasks.forEach(li => {
+                li.style.display = "flex";
+            });
+        }
+        saveData();
+    })
+})
+
 adBtn.addEventListener("click", addTask)
 
 clrBtn.addEventListener("click", ()=>{
-    ul.innerHTML = ""
+    const allTasks = ul.querySelectorAll("li");
+    allTasks.forEach(li => {
+        if (li.style.display !== "none") {
+            ul.removeChild(li);
+        }
+    });
     saveData();
 })
 
