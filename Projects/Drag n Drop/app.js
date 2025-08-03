89a72a033be7cf9e3b5690d6b5609c3b9228c173
fakhir96 <fakhir9101@gmail.com>
2025-08-03 09:11:22 +0500

    Drag and Drop added

---
+++ Projects/Drag n Drop/app.js
@@ -0,0 +1,44 @@
+const cards = document.querySelectorAll(".card");
+const lists = document.querySelectorAll(".list");
+
+cards.forEach(card => {
+    card.addEventListener("dragstart", dragStart);
+    card.addEventListener("dragend", dragEnd);
+});
+
+lists.forEach(list => {
+    list.addEventListener("dragover", dragOver);
+    list.addEventListener("dragenter", dragEnter);
+    list.addEventListener("dragleave", dragLeave);
+    list.addEventListener("drop", dragDrop);
+});
+
+function dragStart(e) {
+    e.dataTransfer.setData("text/plain", this.id)
+}
+
+function dragEnd() {
+    console.log("Drag ENDED");
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.classList.add("over");
+}
+
+function dragLeave(e) {
+    this.classList.remove("over");
+}
+
+function dragDrop(e) {
+    const id = e.dataTransfer.getData("text/plain");
+    const card = document.getElementById(id);
+
+    this.appendChild(card);
+    this.classList.remove("over");
+}
+
