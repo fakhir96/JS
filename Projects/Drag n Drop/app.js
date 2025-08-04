ada4d395eab65d063516951626a1e7fe946222a6
fakhir96 <fakhir9101@gmail.com>
2025-08-04 17:05:06 +0500

    Added more features (DragnDrop)

--- Projects/Drag n Drop/app.js
+++ Projects/Drag n Drop/app.js
@@ -1,26 +1,34 @@
-// const cards = document.querySelectorAll(".card");
 const lists = document.querySelectorAll(".list");
 
 const input1 = document.querySelector("#todo");
+const input2 = document.querySelector("#process");
+const input3 = document.querySelector("#done");
 
 let idCounter = 0;
 
-function addTask(){
+function addTask(input, listIndex){
 
+    // If input is empty so add nothing
+    const text = input.value.trim();
+    if (!text) return;
+
+    // Creating the element and it's id
     let div = document.createElement("div");
     const cardId = `card-${idCounter++}`;
 
+    // Setting All Attributes
     div.classList.add("card");
     div.setAttribute("id", cardId);
     div.setAttribute("draggable", "true");
 
     // Create the text node
-    let taskText = document.createTextNode(input1.value.trim());
+    let taskText = document.createTextNode(text);
 
     // Create the delete icon
     let deleteIcon = document.createElement("i");
     deleteIcon.className = "fa-solid fa-square-minus";
 
+    // Remove card by clicking on icon
     deleteIcon.addEventListener("click", () => {
         div.remove();
     });
@@ -29,20 +37,19 @@ function addTask(){
     div.appendChild(taskText);
     div.appendChild(deleteIcon);
 
+    // Drag and Drop Event Listeners
     div.addEventListener("dragstart", dragStart);
     div.addEventListener("dragend", dragEnd);
 
-    lists[0].appendChild(div);
+    // Adding the card in appropriate list like (1 for process list)
+    lists[listIndex].appendChild(div);
     
-    input1.value = "";
+    // Clearing The Input
+    input.value = "";
+    input.blur();
 }
 
-
-// cards.forEach(card => {
-//     card.addEventListener("dragstart", dragStart);
-//     card.addEventListener("dragend", dragEnd);
-// });
-
+// Add
 lists.forEach(list => {
     list.addEventListener("dragover", dragOver);
     list.addEventListener("dragenter", dragEnter);
@@ -82,8 +89,8 @@ function dragDrop(e) {
 // Add Enter key event listener 
 document.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-      if (input1.value != "") {
-        addTask();
-      }
+        if (input1.value.trim()) addTask(input1, 0);
+        else if (input2.value.trim()) addTask(input2, 1);
+        else if (input3.value.trim()) addTask(input3, 2);
     }
 });
